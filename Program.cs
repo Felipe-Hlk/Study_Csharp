b8b2194f048f3979ea1f42a6a92dd71bfb21ad42
Felipe-Hlk <felipearrudappereira@gmail.com>
2022-11-22 10:51:05 -0300

    second way to write 'hello world'

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
