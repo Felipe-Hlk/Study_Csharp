03957de23254aaff50ce28f624eaeea617518294
Felipe-Hlk <felipearrudappereira@gmail.com>
2022-11-23 12:40:14 -0300

    change same strings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 using System.Globalization;
 
 namespace FirstProject
@@ -7,17 +7,35 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Good Day to Us");
-
-            Console.WriteLine("Another Exercise");
-
             double x = 10.35784;
+            int y = 25;
+            string z = "Bruna";
+            char s = 'F';
+
+            Console.WriteLine();
+            Console.WriteLine(" Hello World! ");
+            Console.WriteLine();
+            Console.WriteLine(" Good Day to Us ");
+            Console.WriteLine();
+            
+            
 
             Console.WriteLine(x);
             Console.WriteLine(x.ToString("F2"));
             Console.WriteLine(x.ToString("F4"));
             Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            Console.WriteLine();
+            Console.WriteLine(" RESULTADO = " + x);
+            Console.WriteLine(" O Valor do troco é: " + x + " reais ");
+            Console.WriteLine(" O Valor do troco é: " + x.ToString("F2") + " reais ");
+
+            Console.WriteLine();
+            Console.WriteLine(" A paciente " + z + " tem " + y + " anos e seu sexo é: " + s);
+
+
+            Console.ReadLine();
         }
     }
 }
