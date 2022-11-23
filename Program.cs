5fbee749da89db7fc7aa9ca38774c301e494838d
Felipe-Hlk <felipearrudappereira@gmail.com>
2022-11-23 14:31:51 -0300

    variable exercises

--- Program.cs
+++ Program.cs
@@ -7,47 +7,16 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
+            int 
+            x = 5;
+
             double
-            x = 10.35784;
-
-            int y, xx, yy;
-            y = 25;
-            xx = 5;
-            yy = 2 * 5;
-
-            string 
-            z = "Bruna";
-
-            char
-            s = 'F';
+            y = 2 * x;
 
             Console.WriteLine();
-            Console.WriteLine(" Hello World! ");
-            Console.WriteLine();
-            Console.WriteLine(" Good Day to Us ");
-            Console.WriteLine();
-            
-            
-
             Console.WriteLine(x);
-            Console.WriteLine(x.ToString("F2"));
-            Console.WriteLine(x.ToString("F4"));
-            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
-
-
             Console.WriteLine();
-            Console.WriteLine(" RESULTADO = " + x);
-            Console.WriteLine(" O Valor do troco é: " + x + " reais ");
-            Console.WriteLine(" O Valor do troco é: " + x.ToString("F2") + " reais ");
-
-            Console.WriteLine();
-            Console.WriteLine(" A paciente " + z + " tem " + y + " anos e seu sexo é: " + s);
-            Console.WriteLine();
-
-            
-            Console.WriteLine(xx);
-            Console.WriteLine();
-            Console.WriteLine(yy);
+            Console.WriteLine(y);
 
 
 
